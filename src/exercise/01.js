36b0f7953d01b38cb6f99ff935c2ffc043ee74d7
Gissel Zamonsky <gissel@gmail.com>
2020-10-22 19:57:54 -0500

    useReducer: extra 1

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,15 +3,15 @@
 
 import React from 'react'
 
-// "state" - the current value of count
-// "newState" - the value passed to setCount
-const countReducer = (state, newState) => newState
+// "count" - the current value of count
+// "step" - the value passed to setCount
+const countReducer = (count, step) => count + step
 
 function Counter({initialCount = 0, step = 1}) {
   //const [state, dispatch] = useReducer(reducer, initialArg, init);
-  const [count, setCount] = React.useReducer(countReducer, initialCount)
+  const [count, changeCount] = React.useReducer(countReducer, initialCount)
+  const increment = () => changeCount(step)
 
-  const increment = () => setCount(count + step)
   return <button onClick={increment}>{count}</button>
 }
 
@@ -20,19 +20,3 @@ function App() {
 }
 
 export default App
-
-// Counter original with useState
-// import React from 'react'
-
-// function Counter({initialCount = 0, step = 1}) {
-//   const [count, setCount] = React.useState(initialCount)
-
-//   const increment = () => setCount(count + step)
-//   return <button onClick={increment}>{count}</button>
-// }
-
-// function App() {
-//   return <Counter />
-// }
-
-// export default App
